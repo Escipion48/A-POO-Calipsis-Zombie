0b7ca3522a4963153c1d2e9609cfccfe9ddd595d
El_0x6861636B <50539327+Escipion48@users.noreply.github.com>
2024-11-28 16:36:29 +0100

    Update Provision.java

--- Provision.java
+++ Provision.java
@@ -10,6 +10,5 @@ public class Provision extends Equipo{
         this.caducidad[1]= caducidad[1];
         this.caducidad[2]= caducidad[2];
         this.tipo = tipo;
-        //Hola, prueba
     }
 }
