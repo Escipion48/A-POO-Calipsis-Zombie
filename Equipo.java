a5ef22460c1e098ad389eeca109cb782d202d456
El_0x6861636B <50539327+Escipion48@users.noreply.github.com>
2024-11-30 22:39:25 +0100

    Update Equipo.java

--- Equipo.java
+++ Equipo.java
@@ -1,7 +1,21 @@
 public abstract class Equipo {
     private String nombre;
-    
+
     public Equipo(String nombre) {
         this.nombre = nombre;
     }
+
+    public Equipo() {
+        this.nombre = "Default";
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
 }
