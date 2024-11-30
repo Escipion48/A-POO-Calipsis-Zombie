9882ae23c6f4451da8ad7a251dde47a4d683de99
El_0x6861636B <50539327+Escipion48@users.noreply.github.com>
2024-12-01 00:10:24 +0100

    Update Zombie.java

--- Zombie.java
+++ Zombie.java
@@ -1,42 +1,97 @@
-public class Zombie extends EntidadActivable{
-    private static int nextId = 1;
-    private int id;
-    private int aguante;
+public class Zombie implements EntidadActivable {
 
-    public Zombie(int acciones,int aguante){
-        super(acciones);
+    private long identificador;
+    private int aguante;
+    private int activaciones;
+    private TipoZombie tipo;
+
+    public Zombie() {
+        this(System.currentTimeMillis(),TipoZombie.CAMINANTE);
+
+    }
+
+    public Zombie (long identificador, TipoZombie tipo){
+        this.identificador = identificador;
+        this.tipo = tipo;
+        if(tipo == TipoZombie.CAMINANTE){
+            this.aguante = 1;
+            this.activaciones = 1;
+        }
+        if(tipo == TipoZombie.CORREDOR){
+            this.aguante = 1;
+            this.activaciones = 2;
+        }
+        if(tipo == TipoZombie.ABOMINACION){
+            this.aguante = 3;
+            this.activaciones = 1;
+        }
+    }
+
+    public Zombie(long identificador, int aguante, int activaciones, TipoZombie tipo) {
+        this.identificador = identificador;
         this.aguante = aguante;
-        this.id = nextId++;
+        this.activaciones = activaciones;
+        this.tipo = tipo;
     }
-    public Zombie(){
-        super(1);
-        this.aguante = 1;
-        this.id = nextId++;
+
+    public long getIdentificador() {
+        return this.identificador;
     }
-    public int getId() {
-        return id;
+
+    public void setIdentificador(long identificador) {
+        this.identificador = identificador;
     }
+
     public int getAguante() {
-        return aguante;
+        return this.aguante;
     }
+
     public void setAguante(int aguante) {
         this.aguante = aguante;
     }
-    @Override
-    public String toString() {
-        return "Zombie[id= " + this.id + ", aguante= " + this.aguante + ", acciones= "+ this.getAcciones()+"]";
+
+    public int getActivaciones() {
+        return this.activaciones;
+    }
+
+    public void setActivaciones(int activaciones) {
+        this.activaciones = activaciones;
+    }
+
+    public TipoZombie getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(TipoZombie tipo) {
+        this.tipo = tipo;
     }
 
     @Override
-    public boolean equals(Object o){
+    public boolean equals(Object o) {
         if(this == o) return true;
-        if(o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Zombie zombie = (Zombie) o;
-        return this.getId() == zombie.getId();
+        return getIdentificador() == zombie.getIdentificador();
+    }
+    
+
+    public void reaccionAtaque() {
+        //TODO: Implementar
     }
 
-    /*public boolean reaccionAtaque(int potencia){
-        return (potencia>=this.getAguante());
-    }*/
+    @Override
+    public void activarse() {
+    //TODO: Implementar
+    }
+
+    @Override
+    public void moverse() {
+    //TODO: Implementar
+    }
+
+    @Override
+    public void atacar() {
+    //TODO: Implementar
+    }
 
 }
