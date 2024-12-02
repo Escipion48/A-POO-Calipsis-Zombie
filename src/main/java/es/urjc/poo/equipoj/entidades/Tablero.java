4f8cbe154fbcbaf45a35c48dcb72c221ba918ab9
Escipion48 <escipio480@gmail.com>
2024-12-02 12:27:55 +0100

    Tablero creado, Comprobar si funciona todo, creo que sobretodo las funciones que usan el array de casillas pordrian fallar

--- src/main/java/es/urjc/poo/equipoj/entidades/Tablero.java
+++ src/main/java/es/urjc/poo/equipoj/entidades/Tablero.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Tablero {
-    Posicion dimensiones;//Los valores máximos del tablero, es decir, un 10x10 tendra como posicion la 9,9;
-    Posicion objetivo; //En esta se incluye la posicion de la casilla final, donde termina el juego, por defecto en un ta blero 10x10 tambien seria la 9,9
-    Casilla [][] tablero;
+    private Posicion dimensiones;//Los valores máximos del tablero, es decir, un 10x10 tendra como posicion la 9,9;
+    private Posicion objetivo; //En esta se incluye la posicion de la casilla final, donde termina el juego, por defecto en un ta blero 10x10 tambien seria la 9,9
+    private Casilla [][] tablero;
 
     public Tablero(Posicion dimensiones, Posicion objetivo, Casilla[][] tablero) {
         this.dimensiones = dimensiones;
