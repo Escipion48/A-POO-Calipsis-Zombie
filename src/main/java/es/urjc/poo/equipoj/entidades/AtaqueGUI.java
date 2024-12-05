8660c29d5071eed7b1ee3cbe60f66ee3154241e1
sKaro05 <298622701q@gmail.com>
2024-12-05 18:10:29 +0100

    forma

---
+++ src/main/java/es/urjc/poo/equipoj/entidades/AtaqueGUI.java
@@ -0,0 +1,13 @@
+package es.urjc.poo.equipoj.entidades;
+
+import javax.swing.*;
+
+public class AtaqueGUI {
+    private JPanel PanelPrincipal;
+    private JLabel selecionaArma;
+    private JLabel selecionaCasilla;
+    private JComboBox listaArmas;
+    private JComboBox listaCasillas;
+    private JButton Atacar;
+    private JTextArea CajaResultado;
+}
